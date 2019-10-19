1fd26b7855de5a0c8812d192f81d832c92589d32
SharkayL <eiikiroenvy@gmail.com>
2019-10-19 23:24:44 +1100

    Initial Setup

---
+++ Assets/Scripts/AllOGPhysics.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AllOGPhysics : MonoBehaviour
+{
+    PlanetPhysics planet;
+    Rigidbody body;
+    Vector3 normal;
+    public float accelaration;
+    // Start is called before the first frame update
+    void Start()
+    {
+        planet = FindObjectOfType<PlanetPhysics>();
+        body = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        normal = this.transform.position - planet.transform.position;
+        body.AddForce(-normal, ForceMode.Acceleration);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow)) {
+            transform.position += -transform.up*accelaration;
+        }
+    }
+}
---
+++ Assets/Scripts/PlanetPhysics.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetPhysics : MonoBehaviour
+{
+    float radius;
+    // Start is called before the first frame update
+    void Start()
+    {
+        radius = this.GetComponent<SphereCollider>().radius;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
