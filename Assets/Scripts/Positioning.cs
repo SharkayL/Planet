38c38e83409d724afe07adb8ae299e42ff230b2e
SharkayL <eiikiroenvy@gmail.com>
2019-10-29 11:42:11 +1100

    Birds

--- Assets/Scripts/Positioning.cs
+++ Assets/Scripts/Positioning.cs
@@ -14,9 +14,8 @@ public class Positioning : MonoBehaviour
         oriPos = this.transform.position;
         Vector3 v = this.transform.position - planet.transform.position;
         transform.up = v.normalized;
-        float dis = Vector3.Distance(this.transform.position, planet.transform.position);
-        Vector3 position = Vector3.Normalize( v) * (planet.radius);
-        transform.position = this.planet.transform.position  + position;
+        pos = Vector3.Normalize(v) * (planet.radius);
+        transform.position = this.planet.transform.position  + pos;
     }
 
 }
---
+++ Assets/Scripts/Turtle.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turtle : MonoBehaviour
+{
+    public bool beingGrabed = false;
+    float timer;
+    bool timing = false;
+    public GameObject theBird;
+    bool beingReleased = false;
+    bool landing = false;
+    Vector3 dir;
+    PlanetPhysics planet;
+    float speed = 8;
+    private void Start()
+    {
+        planet = FindObjectOfType<PlanetPhysics>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (beingGrabed) {
+            transform.SetParent(theBird.transform);
+            beingGrabed = false;
+            timing = true;
+        }
+        if (timing) {
+            timer += Time.deltaTime;
+            if (timer >= 7) {
+                beingReleased = true;
+                timing = false;
+            }
+        }
+        if (beingReleased)
+        {
+            transform.SetParent(null);
+            landing = true;
+            dir = (planet.transform.position - this.transform.position).normalized;
+        }
+    }
+    private void FixedUpdate()
+    {
+        if (landing) {
+            float distance = Vector3.Distance(planet.transform.position, this.transform.position);
+            if (distance > planet.radius)
+            {
+                transform.position += dir * speed * Time.deltaTime;
+            }
+            else {
+                Vector3 normal = -dir;
+                transform.up = normal;
+                transform.position = this.planet.transform.position + normal * (planet.radius);
+                landing = false;
+            }
+        }
+    }
+
+}
